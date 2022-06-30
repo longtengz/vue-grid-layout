2d090535b93c6daae8cad798265124bd58061354
teng <teng@long.com>
2022-06-30 15:41:20 +0800

    fix

--- src/components/GridItem.vue
+++ src/components/GridItem.vue
@@ -446,7 +446,7 @@
         },
         methods: {
             scaleCoordinate: function (coordinate) {
-                return coordinate * this.scale
+                return coordinate / this.scale
             },
             createStyle: function () {
                 if (this.x + this.w > this.cols) {
@@ -612,6 +612,7 @@
                         newPosition.top = this.scaleCoordinate(clientRect.top) - this.scaleCoordinate(parentRect.top);
                         this.dragging = newPosition;
                         this.isDragging = true;
+                        console.log("dragstart", this.dragging)
                         break;
                     }
                     case "dragend": {
@@ -630,6 +631,7 @@
                         this.dragging = null;
                         this.isDragging = false;
                         // shouldUpdate = true;
+                        console.log("dragend", this.dragging)
                         break;
                     }
                     case "dragmove": {
@@ -645,6 +647,7 @@
 //                        console.log("### drag => " + event.type + ", deltaX=" + coreEvent.deltaX + ", deltaY=" + coreEvent.deltaY);
 //                        console.log("### drag end => " + JSON.stringify(newPosition));
                         this.dragging = newPosition;
+                        console.log("dragmove", this.dragging)
                         break;
                     }
                 }
